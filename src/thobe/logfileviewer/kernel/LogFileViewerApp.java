cfb27208ead3b7c4640d12dde5b298c498aa5fb7
Thomas Obenaus <obenaus.thomas@gmail.com>
2014-10-31 19:35:47 +0100

    .UPDATE comment updated

--- src/thobe/logfileviewer/kernel/LogFileViewerApp.java
+++ src/thobe/logfileviewer/kernel/LogFileViewerApp.java
@@ -2,11 +2,12 @@
  *  Copyright (C) 2014, Thomas Obenaus. All rights reserved.
  *  Licensed under the New BSD License (3-clause lic)
  *  See attached license-file.
- *  
+ *
  *	Author: 	Thomas Obenaus
  *	EMail:		obenaus.thomas@gmail.com
- *  Project:    EthTrace
+ *  Project:    LogFileViewer
  */
+
 
 package thobe.logfileviewer.kernel;
 
