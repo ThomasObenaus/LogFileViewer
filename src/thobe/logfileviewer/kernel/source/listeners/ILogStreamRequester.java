c5323a831cca8fec2b58af77bd376322dab3b2ab
Thomas Obenaus <obenaus.thomas@gmail.com>
2014-11-24 14:39:56 +0100

    .ADD ILogStream and ILogStreamRequester added

---
+++ src/thobe/logfileviewer/kernel/source/listeners/ILogStreamRequester.java
@@ -0,0 +1,23 @@
+/*
+ *  Copyright (C) 2014, Thomas Obenaus. All rights reserved.
+ *  Licensed under the New BSD License (3-clause lic)
+ *  See attached license-file.
+ *
+ *	Author: 	Thomas Obenaus
+ *	EMail:		obenaus.thomas@gmail.com
+ *  Project:    LogFileViewer
+ */
+
+package thobe.logfileviewer.kernel.source.listeners;
+
+/**
+ * @author Thomas Obenaus
+ * @source ILogStreamRequester.java
+ * @date Nov 24, 2014
+ */
+public interface ILogStreamRequester
+{
+
+}
+
+
