476fca3fb8cdeed5bc7325e4bdbe50de51a04c06
Thomas Obenaus <obenaus.thomas@gmail.com>
2015-03-21 15:19:24 +0100

    .UPDATE Now plugins will be searched and registered automatically

--- src/thobe/logfileviewer/kernel/plugin/PluginManager.java
+++ src/thobe/logfileviewer/kernel/plugin/PluginManager.java
@@ -10,11 +10,23 @@
 
 package thobe.logfileviewer.kernel.plugin;
 
+import java.io.File;
+import java.io.FilenameFilter;
+import java.io.IOException;
+import java.lang.reflect.Modifier;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.net.URLClassLoader;
+import java.util.ArrayList;
+import java.util.Enumeration;
 import java.util.HashMap;
 import java.util.HashSet;
+import java.util.List;
 import java.util.Map;
 import java.util.Map.Entry;
 import java.util.Set;
+import java.util.jar.JarEntry;
+import java.util.jar.JarFile;
 import java.util.logging.Logger;
 
 import thobe.logfileViewer.plugins.console.Console;
@@ -38,9 +50,125 @@ public class PluginManager implements IPluginAccess, IMemoryWatchable
 		this.plugins = new HashMap<>( );
 	}
 
+	@SuppressWarnings ( "unchecked")
 	public void findAndRegisterPlugins( )
 	{
-		// TODO: implement
+		File pluginDir = new File( "plugins" );
+
+		File[] plugins = pluginDir.listFiles( new FilenameFilter( )
+		{
+
+			@Override
+			public boolean accept( File dir, String name )
+			{
+				if ( name == null )
+					return false;
+				if ( name.trim( ).isEmpty( ) )
+					return false;
+
+				if ( !name.matches( ".*\\.jar$" ) )
+					return false;
+				return true;
+			}
+		} );
+
+		List<JarFile> jarFiles = new ArrayList<JarFile>( );
+
+		// 1. Now build the ULR array for loading the jars to class-path
+		LOG( ).info( "1. Now build the ULR array for loading the jars to class-path." );
+		URL[] pluginUrls = new URL[plugins.length];
+		for ( int i = 0; i < plugins.length; ++i )
+		{
+			try
+			{
+				jarFiles.add( new JarFile( plugins[i] ) );
+				pluginUrls[i] = plugins[i].toURI( ).toURL( );
+				LOG( ).info( "\t'" + pluginUrls[i] + "' will be added to class-path." );
+			}
+			catch ( MalformedURLException e )
+			{
+				LOG( ).severe( "Unable to add '" + pluginUrls[i] + "' to class-path." );
+			}
+			catch ( IOException e )
+			{
+				LOG( ).severe( "Unable to create jar-file from '" + plugins[i] + "' (this one won't be available at class-path)." );
+			}
+		}// for ( int i = 0; i < plugins.length; ++i )
+
+		// 2. Now load the jars to class-path
+		LOG( ).info( "2. Now load the jars to class-path." );
+		URLClassLoader classLoader = new URLClassLoader( pluginUrls );
+
+		// 3. Now find the plugins. 
+		List<Class<? extends Plugin>> pluginClasses = new ArrayList<Class<? extends Plugin>>( );
+		LOG( ).info( "3. Now find the plugins." );
+		for ( JarFile jarFile : jarFiles )
+		{
+			Enumeration<JarEntry> entries = jarFile.entries( );
+			while ( entries.hasMoreElements( ) )
+			{
+				JarEntry entry = entries.nextElement( );
+				String name = entry.getName( );
+				int extIndex = name.lastIndexOf( ".class" );
+				if ( extIndex > 0 )
+				{
+					String plainClassName = name.substring( 0, extIndex );
+					plainClassName = plainClassName.replaceAll( "/", "." );
+
+					try
+					{
+						Class<?> classToLoad = classLoader.loadClass( plainClassName );
+
+						// check if it is a plugin
+						final boolean bImplementsIPlugin = Plugin.class.isAssignableFrom( classToLoad );
+						final boolean bIsAbstract = Modifier.isAbstract( classToLoad.getModifiers( ) );
+						final boolean bIsInterface = classToLoad.isInterface( );
+						if ( bImplementsIPlugin && !bIsAbstract && !bIsInterface )
+						{
+							LOG( ).info( "\tPlugin found: '" + classToLoad.getName( ) + "'" );
+							pluginClasses.add( ( Class<? extends Plugin> ) classToLoad );
+						}
+					}
+					catch ( NoClassDefFoundError e )
+					{
+						LOG( ).warning( "\tUnable to load class " + name + " using '" + plainClassName + "': NoClassDefFoundError '" + e.getLocalizedMessage( ) + "'" );
+					}
+					catch ( ClassNotFoundException e )
+					{
+						LOG( ).warning( "\tUnable to load class " + name + " using '" + plainClassName + "': ClassNotFoundException '" + e.getLocalizedMessage( ) + "'" );
+					}
+
+				}// if ( extIndex > 0 )
+			}// while ( entries.hasMoreElements( ) )
+		}// for ( JarFile jarFile : jarFiles )
+
+		// 4. Now register the plugins. 
+		LOG( ).info( "4. Now register the plugins (" + pluginClasses.size( ) + ")" );
+		for ( Class<? extends Plugin> pluginClass : pluginClasses )
+		{
+			try
+			{
+				Plugin plugin = pluginClass.newInstance( );
+				this.registerPlugin( plugin );
+				LOG( ).info( "\tPlugin '" + plugin.getName( ) + "' sucessfully registered." );
+			}
+			catch ( InstantiationException | IllegalAccessException e )
+			{
+				LOG( ).severe( "\tError creating plugin: " + e.getLocalizedMessage( ) );
+			}
+		}// for ( Class<? extends Plugin> pluginClass : pluginClasses )
+
+		// now finally close the loader 
+		LOG( ).info( "5. Finally close the loader " );
+		try
+		{
+			classLoader.close( );
+		}
+		catch ( IOException e )
+		{
+			LOG( ).severe( "Error closing classLoder: " + e.getLocalizedMessage( ) );
+		}
+
 	}
 
 	public void registerPlugin( Plugin plugin )
