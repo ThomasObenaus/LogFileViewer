178d8416c54c8ea7a316b0e5f7d1bddc51ba7048
Thomas Obenaus <obenaus.thomas@gmail.com>
2015-03-22 18:10:42 +0100

    .RELEASE New version 1.3.0 released

--- src/thobe/logfileviewer/LogFileViewerInfo.java
+++ src/thobe/logfileviewer/LogFileViewerInfo.java
@@ -19,8 +19,8 @@ public class LogFileViewerInfo
 {
 	private static final String	APP_NAME		= "LogFileViewer";
 	private static final int	MAJOR_VERSION	= 1;
-	private static final int	MINOR_VERSION	= 2;
-	private static final int	BUGFIX_VERSION	= 5;
+	private static final int	MINOR_VERSION	= 3;
+	private static final int	BUGFIX_VERSION	= 0;
 
 	public static String getAppName( )
 	{
